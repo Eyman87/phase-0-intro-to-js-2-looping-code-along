f3d6d346b12ad1e215994a8fda6d2d82936492bb
Eyman Eghterafi <eghterafieyman@gmail.com>
2021-12-26 21:31:17 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.lenght; i++) {
+//         console.log(`Wrapped ${gifts[i]} and addeda  bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+function writeCards (names, eventName) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+}
+return newArray
+}
+
+
+let countDown = function(i) {
+    console.log(i);
+    i>0 && countDown(i-1)
+}
+countDown(10)
